9af15bcbcd0664b621ea80101e25780e289ae416
skratchdot <git@skratchdot.com>
2013-08-02 02:28:13 -0400

    initial commit

---
+++ timbre.soundfont.js
@@ -0,0 +1,158 @@
+/*!
+ * timbre.soundfont.js
+ * http://skratchdot.com/projects/timbre.soundfont.js/
+ *
+ * Copyright (c) 2013 skratchdot
+ * Licensed under the MIT license.
+ */
+/*globals timbre */
+(function () {
+	'use strict';
+
+	var soundfont = {},
+		// config
+		maxLoadTime = 1000,
+		// settings
+		urlTemplate = 'https://free-midi.googlecode.com/git/channel/0/instrument/{instrument}/{note}.js?_callback=soundfont',
+		name = 'gm',
+		channel = 0,
+		instrument = 0,
+		length = 64,
+		jsonp = true,
+		// functions
+		ensureNote,
+		getSample,
+		// this is where we cache audio objects (and play/replay them)
+		audioCache = {};
+
+	ensureNote = function (note) {
+		// make sure name exists
+		if (!audioCache.hasOwnProperty(name)) {
+			audioCache[name] = {};
+		}
+		// make sure channel exists
+		if (!audioCache[name].hasOwnProperty(channel)) {
+			audioCache[name][channel] = {};
+		}
+		// make sure instrument exists
+		if (!audioCache[name][channel].hasOwnProperty(instrument)) {
+			audioCache[name][channel][instrument] = {};
+		}
+		// make sure length exists
+		if (!audioCache[name][channel][instrument].hasOwnProperty(length)) {
+			audioCache[name][channel][instrument][length] = {};
+		}
+		// make sure note exists
+		if (!audioCache[name][channel][instrument][length].hasOwnProperty(note)) {
+			audioCache[name][channel][instrument][length][note] = {
+				sample: null,
+				isLoading: false,
+				loadTime: (new Date()).getTime(),
+				callbacks: []
+			};
+		}
+	};
+
+	getSample = function (note, callback) {
+		var audio, url;
+		ensureNote(note);
+		audio = audioCache[name][channel][instrument][length][note];
+		if (audio.sample !== null) {
+			callback(audio.sample, true);
+		} else if (audio.isLoading && (new Date()).getTime() - audio.loadTime < maxLoadTime) {
+			audio.callbacks.push(callback);
+		} else {
+			audio.isLoading = true;
+			try {
+				url = urlTemplate.replace('{name}', name, 'gi')
+						.replace('{channel}', channel, 'gi')
+						.replace('{instrument}', instrument, 'gi')
+						.replace('{length}', length, 'gi')
+						.replace('{note}', note, 'gi');
+
+				timbre('audio' + (jsonp ? '.jsonp' : '')).loadthis(url, function () {
+					var i, cb;
+					audio.sample = this;
+					// execute and empty callbacks
+					for (i = 0; i < audio.callbacks.length; i++) {
+						cb = audio.callbacks[i];
+						if (typeof cb === 'function') {
+							cb(audio.sample, false);
+						}
+					}
+					audio.callbacks = [];
+					audio.isLoading = false;
+					callback(audio.sample, false);
+					// execute global onLoad callback
+					soundfont.onLoad(audio.sample, note);
+				}, function () {
+					// execute global onError 
+					throw new Error('T("audio").loadthis() error.');
+				}).on('ended', function () {
+					this.pause();
+				});
+			} catch (e) {
+				audio.isLoading = false;
+				audio.sample = null;
+				audio.callbacks.push(callback);
+				// execute global onError callback
+				soundfont.onError(e, note);
+			}
+		}
+	};
+
+	soundfont.emptyCache = function () {
+		audioCache = {};
+	};
+
+	soundfont.setUrlTemplate = function (newUrlTemplate) {
+		soundfont.emptyCache();
+		urlTemplate = newUrlTemplate;
+	};
+
+	soundfont.setName = function (newName) {
+		name = newName;
+	};
+
+	soundfont.setChannel = function (newChannel) {
+		channel = newChannel;
+	};
+
+	soundfont.setInstrument = function (newInstrument) {
+		instrument = newInstrument;
+	};
+
+	soundfont.setLength = function (newLength) {
+		length = newLength;
+	};
+
+	soundfont.setJsonp = function (newJsonp) {
+		jsonp = (newJsonp === true) ? true : false;
+	};
+
+	soundfont.preload = function (noteArray) {
+		var i, noop = function () {};
+		for (i = 0; i < noteArray.length; i++) {
+			getSample(noteArray[i], noop);
+		}
+	};
+
+	soundfont.play = function (note, playOnLoad) {
+		playOnLoad = (playOnLoad === false) ? false : true;
+		getSample(note, function (sample, isImmediate) {
+			if (isImmediate || playOnLoad) {
+				sample.play().bang();
+			}
+		});
+	};
+
+	// this can be overridden by end users
+	soundfont.onError = function () {};
+
+	// this can be overridden by end users
+	soundfont.onLoad = function () {};
+
+	if (timbre && timbre.modules && timbre.modules.Decoder && timbre.modules.Decoder.mp3_decode) {
+		timbre.soundfont = soundfont;
+	}
+}());
